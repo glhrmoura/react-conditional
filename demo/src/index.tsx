ce7c3262a764c1648cb7e2e42932295462764a9e
Guilherme Moura <mouraggui@gmail.com>
2023-05-07 11:31:09 -0300

    fix(demo): code adjustments

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,36 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ReactDOM from 'react-dom/client';
 import { If, ElseIf, Else, Condition } from '../../dist/cjs';
 
-const App = ({ user = {} }: { user?: any }) => {
+const App = () => {
+  const [isLogged] = useState();
+  const [isLoading] = useState();
+
   return (
     <React.StrictMode>
       <Condition>
-        <Else>
-          <h2>This text is being rendered by the element {'<Else>'} 1</h2>
-        </Else>
-        <Else>
-          <h2>This text is being rendered by the element {'<Else>'} 2</h2>
-        </Else>
-        <ElseIf condition={false}>
-          <h2>This text is being rendered by the element {'<ElseIf>'} 1</h2>
-        </ElseIf>
-        <ElseIf condition={true}>
-          <h2>This text is being rendered by the element {'<ElseIf>'} 2</h2>
-        </ElseIf>
-        <If condition={false}>
-          <h2>This text is being rendered by the element {'<If>'} 1</h2>
+        <If condition={isLogged}>
+          <h2>The user is logged</h2>
         </If>
-        <Else>
-          <h2>This text is being rendered by the element {'<Else>'} 3</h2>
-        </Else>
-        <ElseIf condition={false}>
-          <h2>This text is being rendered by the element {'<ElseIf>'} 3</h2>
+        <ElseIf condition={isLoading}>
+          <h2>Carregando...</h2>
         </ElseIf>
-        <If condition={user.name}>
-          {() => <h2>This text is being rendered by the element {'<If>'} nome: {user.name.first}</h2>}
-        </If>
+        <Else>
+          <h2>The user is not logged</h2>
+        </Else>
       </Condition>
     </React.StrictMode>
   );
