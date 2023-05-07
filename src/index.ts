6f82da780fa1c308a0b505ccd61057c96dd64feb
Guilherme Moura <mouraggui@gmail.com>
2023-05-07 11:07:57 -0300

    feat: add entry point

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import If from './components/If';
+import Else from './components/Else';
+import ElseIf from './components/ElseIf';
+import Condition from './components/Condition';
+
+export { Condition, If, ElseIf, Else };
