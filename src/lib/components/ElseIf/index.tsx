aa27c9777172744e0dcd3b94c0bf72c4e7f64611
Guilherme Moura <mouraggui@gmail.com>
2023-05-22 11:37:11 -0300

    refactor: library root folder

---
+++ src/lib/components/ElseIf/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface ElseIfProps {
+  condition: any;
+  children: React.ReactNode | (() => React.ReactNode);
+}
+
+const ElseIf: React.FC<ElseIfProps> = ({ children, condition }) => {
+  if (!condition) return null;
+  if (typeof children === 'function') return <>{children()}</>;
+
+  return <>{children}</>;
+};
+
+ElseIf.displayName = 'ElseIf';
+
+export default ElseIf;
