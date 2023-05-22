2f8265eca4ec2a7598e9c285dcc00d45319d7db1
Guilherme Moura <mouraggui@gmail.com>
2023-05-22 11:39:58 -0300

    chore: yarn.lock update

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import If from './components/If';
-import Else from './components/Else';
-import ElseIf from './components/ElseIf';
-import Condition from './components/Condition';
-
-export { Condition, If, ElseIf, Else };
