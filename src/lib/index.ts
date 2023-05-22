7f88974be3b5e6885dfd14114fe031f805ebaeea
Guilherme Moura <mouraggui@gmail.com>
2023-05-22 12:12:56 -0300

    feat: demo page

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import If from '@/lib/components/If';
-import Else from '@/lib/components/Else';
-import ElseIf from '@/lib/components/ElseIf';
-import Condition from '@/lib/components/Condition';
-
-export { Condition, If, ElseIf, Else };
