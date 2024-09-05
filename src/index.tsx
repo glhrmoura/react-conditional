418b944e301a187384b35eb88581ca64319b160f
Guilherme Moura <mouraggui@gmail.com>
2024-09-05 17:49:05 -0300

    feat: update lib

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, ChangeEvent } from 'react';
 
 import ReactDOM from 'react-dom/client';
-import { If, ElseIf, Else, Condition } from '@glhrmoura/react-conditional';
+import { Condition } from '@glhrmoura/react-conditional';
 
 const userTypes = [
   {
@@ -34,13 +34,11 @@ const App = () => {
       <h3>
         User types:
       </h3>
-
       {userTypes.map((type) => (
         <div key={type.value}>
           <label htmlFor={`user-role-${type.value}`}>
             {type.label}:
           </label>
-
           <input
             type="radio"
             id={`user-role-${type.value}`}
@@ -48,25 +46,15 @@ const App = () => {
             value={type.value}
             onChange={onSelectUserType}
             checked={type.value === userType}
-          /> 
+          />
         </div>
       ))}
-
       <hr />
-
       <Condition>
-        <If condition={userType === 'basic'}>
-          <h2>The user is a basic user</h2>
-        </If>
-        <ElseIf condition={userType === 'vip'}>
-          <h2>The user is a vip user</h2>
-        </ElseIf>
-        <ElseIf condition={userType === 'admin'}>
-          <h2>The user is a admin user</h2>
-        </ElseIf>
-        <Else>
-          <h2>The user is not logged</h2>
-        </Else>
+        <h2 rc-if={userType === 'basic'}>The user is a basic user</h2>
+        <h2 rc-else-if={userType === 'vip'}>The user is a vip user</h2>
+        <h2 rc-else-if={userType === 'admin'}>The user is a admin user</h2>
+        <h2 rc-else>The user is not logged</h2>
       </Condition>
     </React.StrictMode>
   );
