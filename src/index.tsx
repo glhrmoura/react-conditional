64325c5d8c7c541f352d658e337b1c3b24d11f06
Guilherme Moura <mouraggui@gmail.com>
2024-09-10 17:36:12 -0300

    feat: update layout

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,13 @@ import React, { useState, ChangeEvent } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Condition } from '@glhrmoura/react-conditional';
 
+import './styles.css';
+
 const userTypes = [
-  {
-    label: 'Basic',
-    value: 'basic',
-  },
-  {
-    label: 'VIP',
-    value: 'vip',
-  },
-  {
-    label: 'Admin',
-    value: 'admin',
-  },
-  {
-    label: 'Logged out',
-    value: '',
-  }
+  { label: 'Basic', value: 'basic' },
+  { label: 'VIP', value: 'vip' },
+  { label: 'Admin', value: 'admin' },
+  { label: 'Logout', value: '' }
 ];
 
 const App = () => {
@@ -31,31 +21,33 @@ const App = () => {
 
   return (
     <React.StrictMode>
-      <h3>
-        User types:
-      </h3>
-      {userTypes.map((type) => (
-        <div key={type.value}>
-          <label htmlFor={`user-role-${type.value}`}>
-            {type.label}:
-          </label>
-          <input
-            type="radio"
-            id={`user-role-${type.value}`}
-            name="user-type"
-            value={type.value}
-            onChange={onSelectUserType}
-            checked={type.value === userType}
-          />
+      <div className="container">
+        <h3>User types:</h3>
+        <div className="user-type-container">
+          {userTypes.map((type) => (
+            <div className="user-type" key={type.value}>
+              <input
+                type="radio"
+                id={`user-role-${type.value}`}
+                name="user-type"
+                value={type.value}
+                onChange={onSelectUserType}
+                checked={type.value === userType}
+              />
+              <label htmlFor={`user-role-${type.value}`}>{type.label}</label>
+            </div>
+          ))}
         </div>
-      ))}
-      <hr />
-      <Condition>
-        <h2 rc-if={userType === 'basic'}>The user is a basic user</h2>
-        <h2 rc-else-if={userType === 'vip'}>The user is a vip user</h2>
-        <h2 rc-else-if={userType === 'admin'}>The user is a admin user</h2>
-        <h2 rc-else>The user is not logged</h2>
-      </Condition>
+        <hr />
+        <div className="result-container">
+          <Condition>
+            <h2 rc-if={userType === 'basic'}>The user is a basic</h2>
+            <h2 rc-else-if={userType === 'vip'}>The user is a vip</h2>
+            <h2 rc-else-if={userType === 'admin'}>The user is an admin</h2>
+            <h2 rc-else>There is no user</h2>
+          </Condition>
+        </div>
+      </div>
     </React.StrictMode>
   );
 };
