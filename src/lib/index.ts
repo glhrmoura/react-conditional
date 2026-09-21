152020d33dc798ed74a124480e4315f22a965f93
Guilherme Moura <glhrmoura@gmail.com>
2026-09-21 12:53:55 -0300

    feat: export conditional component prop types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,15 @@
 import { Condition, If, ElseIf, Else } from './components/Condition';
+import type { ConditionProps, IfProps, ElseIfProps, ElseProps } from './components/Condition';
 import { Switch, Match, Default } from './components/Switch';
+import type { SwitchProps, MatchProps, DefaultProps } from './components/Switch';
 
 export { Condition, If, ElseIf, Else, Switch, Match, Default };
+export type {
+  ConditionProps,
+  IfProps,
+  ElseIfProps,
+  ElseProps,
+  SwitchProps,
+  MatchProps,
+  DefaultProps,
+};
