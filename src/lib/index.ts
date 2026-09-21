bc437309424a5fae17e52509cc1e9610fb1e6177
Guilherme Moura <glhrmoura@gmail.com>
2026-09-21 12:51:40 -0300

    feat: export Switch Match and Default

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 import { Condition, If, ElseIf, Else } from './components/Condition';
+import { Switch, Match, Default } from './components/Switch';
 
-export { Condition, If, ElseIf, Else };
+export { Condition, If, ElseIf, Else, Switch, Match, Default };
