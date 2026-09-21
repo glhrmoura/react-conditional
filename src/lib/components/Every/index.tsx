72dae75686a56db9d9c7b9773964026e9394f0bd
Guilherme Moura <glhrmoura@gmail.com>
2026-09-21 14:01:20 -0300

    feat: add Every all-cases boolean component

---
+++ src/lib/components/Every/index.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+import { resolveChildren } from '../../utils/resolve-children';
+
+export interface EveryProps {
+  cases: boolean[];
+  children: ReactNode | (() => ReactNode);
+  fallback?: ReactNode | (() => ReactNode);
+}
+
+const Every: React.FC<EveryProps> = ({ cases, children, fallback }) => {
+  if (cases.every(Boolean)) {
+    return <>{resolveChildren(children)}</>;
+  }
+  if (fallback !== undefined) {
+    return <>{resolveChildren(fallback)}</>;
+  }
+  return null;
+};
+
+export { Every };
