2922fb45317554bd194286c043b5c1c966c3958c
Guilherme Moura <glhrmoura@gmail.com>
2026-09-21 14:01:21 -0300

    feat: add Some any-case boolean component

---
+++ src/lib/components/Some/index.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+import { resolveChildren } from '../../utils/resolve-children';
+
+export interface SomeProps {
+  cases: boolean[];
+  children: ReactNode | (() => ReactNode);
+  fallback?: ReactNode | (() => ReactNode);
+}
+
+const Some: React.FC<SomeProps> = ({ cases, children, fallback }) => {
+  if (cases.some(Boolean)) {
+    return <>{resolveChildren(children)}</>;
+  }
+  if (fallback !== undefined) {
+    return <>{resolveChildren(fallback)}</>;
+  }
+  return null;
+};
+
+export { Some };
