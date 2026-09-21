a0a9b1a702341f5ea5435fac664acfcefd49770f
Guilherme Moura <glhrmoura@gmail.com>
2026-09-21 14:14:01 -0300

    refactor: switch module imports to @/lib alias

--- src/lib/components/Condition/index.tsx
+++ src/lib/components/Condition/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode, useEffect } from 'react';
-import { resolveChildren } from '../../utils/resolve-children';
-import { warnDev } from '../../utils/warn-dev';
+import { resolveChildren } from '@/lib/utils/resolve-children';
+import { warnDev } from '@/lib/utils/warn-dev';
 
 export interface ConditionProps {
   children: ReactNode;
